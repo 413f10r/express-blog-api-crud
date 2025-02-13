1e853882cfee3a1a4bb60bf3b6bff70aaa443e52
413f10r <ale-fioretti@live.it>
2025-02-13 18:36:40 +0100

    first commit

---
+++ routers/postRouter.js
@@ -0,0 +1,19 @@
+
+const { index, show, store, update, modify, destroy } = require("../controllers/postController")
+
+const express = require("express");
+const router = express.Router();
+// index
+router.get('/', index);
+// show
+router.get('/:id', show);
+// store
+router.post('/', store);
+// update
+router.put('/:id', update);
+// modify
+router.patch('/:id', modify);
+// destroy
+router.delete('/:id', destroy);
+
+module.exports = router
